2b5e20eca3bc0d6dc2eefad0d20bdfcbfaca005b
f87m19 <f87m19@hotmail.com>
2020-02-27 01:39:01 +0100

    Update openaps-install.sh

--- bin/openaps-install.sh
+++ bin/openaps-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-BRANCH=${1:-master}
+BRANCH=${1:-v0.6.3}
 read -p "Enter your rig's new hostname (this will be your rig's "name" in the future, so make sure to write it down): " -r
 myrighostname=$REPLY
 echo $myrighostname > /etc/hostname
@@ -70,8 +70,8 @@ fi
 sed -i "s/daily/hourly/g" /etc/logrotate.conf
 sed -i "s/#compress/compress/g" /etc/logrotate.conf
 
-curl -s https://raw.githubusercontent.com/openaps/oref0/$BRANCH/bin/openaps-packages.sh | bash -
-mkdir -p ~/src; cd ~/src && ls -d oref0 && (cd oref0 && git checkout $BRANCH && git pull) || git clone git://github.com/openaps/oref0.git
+curl -s https://raw.githubusercontent.com/openaps/f87m19/$BRANCH/bin/openaps-packages.sh | bash -
+mkdir -p ~/src; cd ~/src && ls -d oref0 && (cd oref0 && git checkout $BRANCH && git pull) || git clone git://github.com/f87m19/oref0.git
 echo "Press Enter to run oref0-setup with the current release ($BRANCH branch) of oref0,"
 read -p "or press ctrl-c to cancel. " -r
 cd && ~/src/oref0/bin/oref0-setup.sh
